8dd145827eb826a71fcd1652a314c13176146f7c
uttam kamar <kamaruttam@gmail.com>
2023-11-15 12:57:20 +0530

    website template

---
+++ js/custom.js
@@ -0,0 +1,138 @@
+// transparent Header
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 1) {
+      $("#header").addClass("sticky-header");
+    } else {
+      $("#header").removeClass("sticky-header");
+    }
+  });
+});
+
+// our air filter
+$(document).ready(function () {
+  $(".ex-img").slick({
+    dots: false,
+    arrows: true,
+    autoplay: false,
+    infinite: false,
+    centerMode: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+// industry serve slider
+$(document).ready(function () {
+  $(".industry-slider").slick({
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1008,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
+
+// logo slider
+$(document).ready(function () {
+  $(".logo-slider").slick({
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    infinite: true,
+    centerMode: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1008,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Responsive Website 🕸️🕸️🕸️
+
+- custom website template, using HTML, CSS & JAVASCRIPT.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+## Screenshots
+
+![Landing-page](./images/screenshot.png)
